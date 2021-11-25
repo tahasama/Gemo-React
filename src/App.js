d7839c4a0421aca6a3f34b6cfabf31bd2d7689c3
tahasama <taha.maatof@gmail.com>
2021-11-25 14:56:19 +0100

    app design

--- src/App.js
+++ src/App.js
@@ -2,8 +2,76 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>Repository ranking app</p>
+    <div>
+      <div className="screen">
+        <img
+          className="avatar"
+          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
+        />
+        <div className="infos">
+          <div className="upper-infos">
+            <h3 className="texty">repository name</h3>
+            <p className="texty">
+              description: description: description: description: description:
+            </p>
+          </div>
+          <div className="bottom-infos">
+            <div className="row1">
+              <p className="boxed">Stars: 100</p>
+              <p className="boxed">Issues: 100</p>
+            </div>
+            <div className="row2">
+              <p>Submitted by repository owner</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="screen">
+        <img
+          className="avatar"
+          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
+        />
+        <div className="infos">
+          <div className="upper-infos">
+            <h3 className="texty">repository name</h3>
+            <p className="texty">
+              description: description: description: description: description:
+              description: description: description: description: description:
+              description:
+            </p>
+          </div>
+          <div className="bottom-infos">
+            <div className="row1">
+              <p className="boxed">Stars: 100</p>
+              <p className="boxed">Issues: 100</p>
+            </div>
+            <div className="row2">
+              <p>Submitted by repository owner</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="screen">
+        <img
+          className="avatar"
+          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
+        />
+        <div className="infos">
+          <div className="upper-infos">
+            <h3 className="texty">repository name</h3>
+            <p className="texty"></p>
+          </div>
+          <div className="bottom-infos">
+            <div className="row1">
+              <p className="boxed">Stars: 100</p>
+              <p className="boxed">Issues: 100</p>
+            </div>
+            <div className="row2">
+              <p>Submitted by repository owner</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
