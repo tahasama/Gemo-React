5be1aa6889e0f22e1ce6b053361c4de0342daf8d
tahasama <taha.maatof@gmail.com>
2021-11-25 15:15:14 +0100

    react hooks and functionalities

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Gemo-React
+
+This app ranks Github repository according to their number of stars
+
+Axios was used to get data from from API, "npm i axios"
+
+Timeago-react was used to get time since the repository was created, "npm install timeago-react"
--- src/App.js
+++ src/App.js
@@ -1,77 +1,76 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import TimeAgo from "timeago-react";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [message, setmessage] = useState(false);
+
+  // Fires first page of API and then at page updating
+  useEffect(() => {
+    getUsers();
+  }, [page]);
+
+  // get data from API and update rpository array
+  const getUsers = async () => {
+    const res = await axios
+      .get(
+        `https://api.github.com/search/repositories?q=created:2017-10-22&sort=stars&order=desc&page=${page}`
+      )
+      .then((res) => setRepos([...repos, ...res.data.items]))
+      .catch((err) => setmessage(true));
+
+    setLoading(false);
+  };
+
+  // Fires once scroll arrives to the end of page, then go to next page
+  const scrollToEnd = () => {
+    setPage(page + 1);
+    setLoading(true);
+  };
+
+  // Detects the end of page while scrolling
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd();
+    }
+  };
+
   return (
     <div>
-      <div className="screen">
-        <img
-          className="avatar"
-          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
-        />
-        <div className="infos">
-          <div className="upper-infos">
-            <h3 className="texty">repository name</h3>
-            <p className="texty">
-              description: description: description: description: description:
-            </p>
-          </div>
-          <div className="bottom-infos">
-            <div className="row1">
-              <p className="boxed">Stars: 100</p>
-              <p className="boxed">Issues: 100</p>
+      {repos.map((repository) => (
+        <div className="screen" key={repository.id}>
+          <img className="avatar" src={repository.owner.avatar_url} />
+          <div className="infos">
+            <div className="upper-infos">
+              <h3 className="texty">{repository.name}</h3>
+              <p className="texty">{repository.description}</p>
             </div>
-            <div className="row2">
-              <p>Submitted by repository owner</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="screen">
-        <img
-          className="avatar"
-          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
-        />
-        <div className="infos">
-          <div className="upper-infos">
-            <h3 className="texty">repository name</h3>
-            <p className="texty">
-              description: description: description: description: description:
-              description: description: description: description: description:
-              description:
-            </p>
-          </div>
-          <div className="bottom-infos">
-            <div className="row1">
-              <p className="boxed">Stars: 100</p>
-              <p className="boxed">Issues: 100</p>
-            </div>
-            <div className="row2">
-              <p>Submitted by repository owner</p>
+            <div className="bottom-infos">
+              <div className="row1">
+                <p className="boxed">Stars: {repository.stargazers_count}</p>
+                <p className="boxed">Issues: {repository.open_issues_count}</p>
+              </div>
+              <div className="row2">
+                <p>
+                  Submitted &nbsp;
+                  <TimeAgo datetime={repository.created_at} />
+                  &nbsp; by {repository.owner.login}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="screen">
-        <img
-          className="avatar"
-          src="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png"
-        />
-        <div className="infos">
-          <div className="upper-infos">
-            <h3 className="texty">repository name</h3>
-            <p className="texty"></p>
-          </div>
-          <div className="bottom-infos">
-            <div className="row1">
-              <p className="boxed">Stars: 100</p>
-              <p className="boxed">Issues: 100</p>
-            </div>
-            <div className="row2">
-              <p>Submitted by repository owner</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
+
+      {loading && <p className="loading">...loading</p>}
+      {message && <p className="loading">...you have reached the limit </p>}
     </div>
   );
 }
